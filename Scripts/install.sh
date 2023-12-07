31e1a2e4ec9fc552f5c9f6044ca69f8694cc6ed6
ekahPruthvi <136414172+ekahPruthvi@users.noreply.github.com>
2023-12-07 17:02:42 +0530

    tha installa scryptsss

---
+++ Scripts/install.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+#|---/ /+--------------------------+---/ /|#
+#|--/ /-| Main installation script |--/ /-|#
+#|-/ /--| @ekah cynageO            |-/ /--|#
+#|/ /---+--------------------------+/ /---|#
+
+cat << "EOF"
+
+-----------------------------------------------------------------
+   ░█▀▄░█░░█░█▀▀▄░█▀▀▄░█▀▀▀░█▀▀░▒█▀▀▀█░▒█▀▀▀█
+   ░█░░░█▄▄█░█░▒█░█▄▄█░█░▀▄░█▀▀░▒█░░▒█░░▀▀▀▄▄
+   ░▀▀▀░▄▄▄▀░▀░░▀░▀░░▀░▀▀▀▀░▀▀▀░▒█▄▄▄█░▒█▄▄▄█     
+-----------------------------------------------------------------
+
+EOF
+
+
+#--------------------------------#
+# import variables and functions #
+#--------------------------------#
+source global_fn.sh
+if [ $? -ne 0 ] ; then
+    echo "Error: unable to source global_fn.sh, please execute from $(dirname $(realpath $0))..."
+    exit 1
+fi
+
+
+#------------------#
+# evaluate options #
+#------------------#
+flg_Install=0
+flg_Restore=0
+flg_Service=0
+
+while getopts idrs RunStep
+do
+    case $RunStep in
+    i) flg_Install=1 ;;
+    d) flg_Install=1
+        export use_default="--noconfirm" ;;
+    r) flg_Restore=1 ;;
+    s) flg_Service=1 ;;
+    *)  echo "...valid options are..."
+        echo "i : [i]nstall hyprland without configs"
+        echo "r : [r]estore config files"
+        echo "s : start system [s]ervices"   
+        exit 1 ;;
+    esac
+done
+
+if [ $OPTIND -eq 1 ] ; then
+    flg_Install=1
+    flg_Restore=1
+    flg_Service=1
+fi
+
+
+#------------#
+# installing #
+#------------#
+if [ $flg_Install -eq 1 ] ; then
+cat << "EOF"
+
+ _         _       _ _ _         
+|_|___ ___| |_ ___| | |_|___ ___ 
+| |   |_ -|  _| .'| | | |   | . |
+|_|_|_|___|_| |__,|_|_|_|_|_|_  |
+                            |___|
+
+EOF
+
+    #----------------------#
+    # prepare package list #
+    #----------------------#
+    shift $((OPTIND -1))
+    cust_pkg=$1
+    cp custom_hypr.lst install_pkg.lst
+
+    if [ -f "$cust_pkg" ] && [ ! -z "$cust_pkg" ] ; then
+        cat $cust_pkg >> install_pkg.lst
+    fi
+
+    #--------------------------------#
+    # add nvidia drivers to the list #
+    #--------------------------------#
+    if nvidia_detect ; then
+
+        cat /usr/lib/modules/*/pkgbase | while read krnl
+        do
+            echo "${krnl}-headers" >> install_pkg.lst
+        done
+
+        echo -e "nvidia-dkms\nnvidia-utils" >> install_pkg.lst
+        sed -i "s/^hyprland-git/hyprland-nvidia-git/g" install_pkg.lst
+
+    else
+        echo "nvidia card not detected, skipping nvidia drivers..."
+    fi
+
+    #--------------------------------#
+    # install packages from the list #
+    #--------------------------------#
+    ./install_pkg.sh install_pkg.lst
+    rm install_pkg.lst
+fi
+
+
+#---------------------------#
+# restore my custom configs #
+#---------------------------#
+if [ $flg_Restore -eq 1 ] ; then
+cat << "EOF"
+
+             _           _         
+ ___ ___ ___| |_ ___ ___|_|___ ___ 
+|  _| -_|_ -|  _| . |  _| |   | . |
+|_| |___|___|_| |___|_| |_|_|_|_  |
+                              |___|
+
+EOF
+
+    ./restore_fnt.sh
+    ./restore_cfg.sh
+fi
+
+
+#---------------------------#
+# update sddm, grub and zsh #
+#---------------------------#
+if [ $flg_Install -eq 1 ] && [ $flg_Restore -eq 1 ] ; then
+    ./restore_sgz.sh
+fi
+
+
+#------------------------#
+# enable system services #
+#------------------------#
+if [ $flg_Service -eq 1 ] ; then
+cat << "EOF"
+
+                 _             
+ ___ ___ ___ _ _|_|___ ___ ___ 
+|_ -| -_|  _| | | |  _| -_|_ -|
+|___|___|_|  \_/|_|___|___|___|
+
+EOF
+
+    service_ctl NetworkManager
+    service_ctl bluetooth
+    service_ctl sddm
+fi
+
---
+++ Scripts/install_pkg.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#|---/ /+----------------------------------------+---/ /|#
+#|--/ /-| Script to install pkgs from input list |--/ /-|#
+#|-/ /--| Prasanth Rangan                        |-/ /--|#
+#|/ /---+----------------------------------------+/ /---|#
+
+source global_fn.sh
+if [ $? -ne 0 ] ; then
+    echo "Error: unable to source global_fn.sh, please execute from $(dirname $(realpath $0))..."
+    exit 1
+fi
+
+if ! pkg_installed git
+    then
+    echo "installing dependency git..."
+    sudo pacman -S git
+fi
+
+chk_aurh
+
+if [ -z $aurhlpr ]
+    then
+    echo -e "Select aur helper:\n1) yay\n2) paru"
+    read -p "Enter option number : " aurinp
+
+    case $aurinp in
+    1) aurhlpr="yay" ;;
+    2) aurhlpr="paru" ;;
+    *) echo -e "...Invalid option selected..."
+        exit 1 ;;
+    esac
+
+    echo "installing dependency $aurhlpr..."
+    ./install_aur.sh $aurhlpr 2>&1
+fi
+
+install_list="${1:-install_pkg.lst}"
+
+while read pkg
+do
+    if [ -z $pkg ]
+        then
+        continue
+    fi
+
+    if pkg_installed ${pkg}
+        then
+        echo "skipping ${pkg}..."
+
+    elif pkg_available ${pkg}
+        then
+        echo "queueing ${pkg} from arch repo..."
+        pkg_arch=`echo $pkg_arch ${pkg}`
+
+    elif aur_available ${pkg}
+        then
+        echo "queueing ${pkg} from aur..."
+        pkg_aur=`echo $pkg_aur ${pkg}`
+
+    else
+        echo "error: unknown package ${pkg}..."
+    fi
+done < <( cut -d '#' -f 1 $install_list )
+
+if [ `echo $pkg_arch | wc -w` -gt 0 ]
+    then
+    echo "installing $pkg_arch from arch repo..."
+    sudo pacman ${use_default} -S $pkg_arch
+fi
+
+if [ `echo $pkg_aur | wc -w` -gt 0 ]
+    then
+    echo "installing $pkg_aur from aur..."
+    $aurhlpr ${use_default} -S $pkg_aur
+fi
