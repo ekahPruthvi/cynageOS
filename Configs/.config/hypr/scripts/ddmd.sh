f2f21f9111b4279957bd9b8eafd49698657954e0
ekahPruthvi <136414172+ekahPruthvi@users.noreply.github.com>
2024-07-16 21:59:59 +0530

    Add files via upload
    nvim to vim

--- Configs/.config/hypr/scripts/ddmd.sh
+++ Configs/.config/hypr/scripts/ddmd.sh
@@ -4,12 +4,12 @@
 ok=$(printf "Mon\nTue\nWed\nThur\nFri\nSat\nSun" | rofi -dmenu -i -theme-str '@import "ddmd.rasi"')
 
 case "$ok" in
-	"Mon") kitty nvim ~/.config/hypr/scripts/Deadline_Doomsday/Monday.txt;;
-	"Tue") kitty nvim ~/.config/hypr/scripts/Deadline_Doomsday/Tuesday.txt ;;
-	"Wed") kitty nvim ~/.config/hypr/scripts/Deadline_Doomsday/Wednesday.txt ;;
-	"Thur") kitty nvim ~/.config/hypr/scripts/Deadline_Doomsday/Thursday.txt ;;
-  "Fri") kitty nvim ~/.config/hypr/scripts/Deadline_Doomsday/Friday.txt ;;
-  "Sat") kitty nvim ~/.config/hypr/scripts/Deadline_Doomsday/Saturday.txt ;;
-  "Sun") kitty nvim ~/.config/hypr/scripts/Deadline_Doomsday/Sunday.txt ;;
+	"Mon") kitty vim ~/.config/hypr/scripts/Deadline_Doomsday/Monday.txt;;
+	"Tue") kitty vim ~/.config/hypr/scripts/Deadline_Doomsday/Tuesday.txt ;;
+	"Wed") kitty vim ~/.config/hypr/scripts/Deadline_Doomsday/Wednesday.txt ;;
+	"Thur") kitty vim ~/.config/hypr/scripts/Deadline_Doomsday/Thursday.txt ;;
+  "Fri") kitty vim ~/.config/hypr/scripts/Deadline_Doomsday/Friday.txt ;;
+  "Sat") kitty vim ~/.config/hypr/scripts/Deadline_Doomsday/Saturday.txt ;;
+  "Sun") kitty vim ~/.config/hypr/scripts/Deadline_Doomsday/Sunday.txt ;;
   *) exit 1 ;;
 esac
