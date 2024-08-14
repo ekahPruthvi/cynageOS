14fb12d5a32558792bed4a5856970825a42a873c
ekahPruthvi <136414172+ekahPruthvi@users.noreply.github.com>
2024-08-14 10:11:55 +0530

    dashboard for update .4

--- Configs/.config/hypr/scripts/Dboard.sh
+++ Configs/.config/hypr/scripts/Dboard.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-kitty btop &
+kitty vim ~/.config/hypr/scripts/Deadline_Doomsday/ded.txt &
 sleep 1s && kitty lavat &
-sleep 2s && kitty peaclock &
-sleep 3s && kitty cal &
+sleep 2s && kitty peaclock 
