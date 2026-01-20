680dc2f16eca804eb3577e360e3bd02f39f60984
ekahPruthvi <p1sribhat@gmail.com>
2026-01-20 10:28:12 +0530

    add niri and dependencies

--- prep_pkg.sh
+++ prep_pkg.sh
@@ -5,17 +5,17 @@
 PKGS=(
   archiso ark bc blueman brightnessctl cliphist code cpio
   dnsmasq dosfstools dunst efibootmgr espeak-ng eza ffmpegthumbs flatpak fuse2
-  greetd grimblast-git grub gst-plugin-pipewire gst-plugins-base gtk4-layer-shell
-  hostapd hyprland hyprpicker hyprshot i2c-tools imagemagick iw jq kde-cli-tools
+  greetd grimblast-git grub gst-plugin-pipewire gst-plugins-base
+  hostapd i2c-tools imagemagick iw jq kde-cli-tools
   kdeconnect kitty kvantum kvantum-qt5 libconfig libev libnotify libresprite linux-headers
   loupe man-db mpv mtools nautilus netdiscover network-manager-applet
-  noto-fonts-emoji oh-my-zsh-git pacman-contrib pamixer parallel pavucontrol
+  noto-fonts-emoji oh-my-zsh-git niri pacman-contrib pamixer parallel pavucontrol
   pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse playerctl pokemon-colorscripts-git
   polkit-kde-agent pv pyprland qt5-graphicaleffects qt5-imageformats qt5-quickcontrols qt5-quickcontrols2
   qt5-wayland qt5ct qt6-virtualkeyboard qt6-wayland qt6ct rpi-imager rustup sddm slurp smassh-bin
   sox swww system-config-printer systemd tk ttf-bigblueterminal-nerd ttf-heavydata-nerd udiskie
   usbutils uthash v4l-utils min-browser-bin vte4 wev wf-recorder wget wireplumber
-  xdg-desktop-portal-gtk xdg-desktop-portal-hyprland xf86-video-vesa xorg-bdftopcf
+  xdg-desktop-portal-gtk xdg-desktop-portal-gnome gnome-keyring xf86-video-vesa xorg-bdftopcf
   xorg-docs xorg-font-util xorg-fonts-100dpi xorg-fonts-75dpi xorg-iceauth xorg-mkfontscale
   xorg-server-devel xorg-server-xephyr xorg-server-xnest xorg-server-xvfb xorg-sessreg xorg-smproxy
   xorg-x11perf xorg-xbacklight xorg-xcmsdb xorg-xcursorgen xorg-xdpyinfo xorg-xdriinfo xorg-xev
